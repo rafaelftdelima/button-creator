8b4e5ee65d060dd93c37e2f068ef7799c444565a
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-23 20:51:14 -0300

    Implement controls' solution

---
+++ script.js
@@ -0,0 +1,48 @@
+const controls = document.querySelector('.controls');
+const css = document.querySelector('.css');
+const button = document.querySelector('.button');
+
+const handleStyle = {
+    element: button,
+    text(value) {
+        this.element.innerText = value;
+    },
+    color(value) {
+        this.element.style.color = value;
+    },
+    backgroundColor(value) {
+        this.element.style.backgroundColor = value;
+    },
+    height(value) {
+        this.element.style.height = value + 'px';
+    },
+    width(value) {
+        this.element.style.width = value + 'px';
+    },
+    border(value) {
+        this.element.style.border = value;
+    },
+    borderRadius(value) {
+        this.element.style.borderRadius = value + 'px';
+    },
+    fontFamily(value) {
+        this.element.style.fontFamily = value;
+    },
+    fontSize(value) {
+        this.element.style.fontSize = value + 'px';
+    }
+}
+
+function showCSS() {
+    css.innerHTML = '<span>' + button.style.cssText.split('; ').join(';</span><span>');
+}
+
+function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    handleStyle[name](value);
+    showCSS();
+}
+
+controls.addEventListener('change', handleChange);
